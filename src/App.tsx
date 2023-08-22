7ad58b718ee2f3aec0fdab272b787340b9a7d7dd
8lag <redcorder.yahata@gmail.com>
2023-08-22 12:49:12 +0900

    復習用に最低限のテンプレートを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,74 @@
 import React, { useState } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import './App.css'
-import Header from './components/header/Header'
-import TaskCards from './components/task/TaskCards'
+import { v4 as uuidv4 } from 'uuid'
+/*
+<DragDropContext>で囲む
+- onDragEndを追加
+<Droppable>を生やす（childrenが必要のエラーを回避）
+- droppableIdを生やす（これは必須） ・・・　複数のドロップ可能エリアを作るときに識別子がないとconflictするから
+- {(provided) => <div></div>} を生やす (以下が必要)
+  - provided.droppableProps
+  - innerRef
+<Draggable>を生やす
+- 注意) draggableIdはuniqueなものを与えないといけない
+- {(provided) => <div></div>} を生やす (以下が必要)
+  - provided.draggableProps
+  - provided.dragHandleProps
+  - innerRef
+*/
+const TaskCard = ({
+  text,
+  index,
+  draggableId,
+}: {
+  text: string
+  index: number
+  draggableId: string
+}) => (
+  <Draggable index={index} draggableId={draggableId}>
+    {(provided) => (
+      <div
+        className="playground-taskCard"
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        ref={provided.innerRef}
+      >
+        <button>{text}</button>
+      </div>
+    )}
+  </Draggable>
+)
 
 function App() {
+  const handleDragEnd = () => {
+    // ここにsetListとかを記述して、dragEnd時にstateを固定する
+  }
+  const tasksList = [...Array(3)].map((e, i) => ({
+    id: uuidv4(),
+    draggableId: `item-${uuidv4()}`,
+    text: `task-${i}`,
+  }))
+
   return (
     <div className="app">
-      <Header />
-      <TaskCards />
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {tasksList.map((task, index) => (
+                <TaskCard
+                  index={index}
+                  text={task.text}
+                  key={task.id}
+                  draggableId={task.draggableId}
+                />
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
